706679cfd81caddbd89d3ffe204f7f06d8f36703
kaiofprates <kaioprates@hotmail.com>
2020-06-04 00:03:02 -0300

    add all tests

--- test/gene.test.js
+++ test/gene.test.js
@@ -1,4 +1,13 @@
-const Rand  = require('../src/randomGene')
+const Rand  = require('../src/randomGene');
+const Gene = require('../src/distance');
+
+const distance = [
+    [0],
+    [20,0],
+    [20,30,0]
+]
+
+let gene = new Gene(distance);
 
 function testRand(){
     let setRand = new Set(Rand())
@@ -8,4 +17,13 @@ function testRand(){
 
 test('Test randomic function', () => {
     expect(testRand()).toBe(9);
+  });
+
+test('Test Gene Class - Get Distance', () => {
+    expect(gene.getDistance(0,1)).toBe(20);
+  });
+
+test('Test Gene Class - Get Path', () => {
+    let {total} = gene.sumPath([0,1,2])
+    expect(total).toBe(50);
   });
